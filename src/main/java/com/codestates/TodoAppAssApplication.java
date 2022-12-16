0e81b56654614c3369096000d700698e4b734a73
parkjiyun98 <dysm0084@gmail.com>
2022-12-16 11:32:00 +0900

    finish

---
+++ src/main/java/com/codestates/TodoAppAssApplication.java
@@ -0,0 +1,13 @@
+package com.codestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppAssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppAssApplication.class, args);
+	}
+
+}
